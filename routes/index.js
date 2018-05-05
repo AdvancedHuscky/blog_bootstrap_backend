5201786a2000f02f254a21379b2312cbd4c068d2
William <menzongwei@126.com>
2018-05-05 08:12:04 +0800

    login page ing

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,14 @@
 module.exports = function (app) {
+    //首页
     app.get('/',function (req,res,next) {
-        res.render('index',{
+        res.render('page_index',{
             title:'首页',
         });
+    })
+    //登陆页面
+    app.get('/login',(req,res,next)=>{
+        res.render('page_login',{
+            title:'登陆',
+        })
     })
 }
