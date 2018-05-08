3e4cb1f9b217f57882304d63fbae88de98dc4798
William <menzongwei@126.com>
2018-05-08 11:20:36 +0800

    mongo connection

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ let express = require('express');
 let app = express();
 //加载模板处理模块
 let swig = require('swig');
+//加载数据库模块
+const mongoose = require('mongoose');
+
 
 //用户通过URL来访问web应用，web后端根据用户访问的url处理不同的业务逻辑
 
@@ -20,7 +23,7 @@ app.engine('html',swig.renderFile);
 //设置模板文件存放的目录，第一个参数必须是views，第二个参数是目录
 app.set('views','./views');
 //注册所使用的模板引擎，第一个参数必须是view engine，第二个参数和app.engine这个方法中定义的模板引擎的名称（第一个参数）一致
-app.set('view engine',html);
+app.set('view engine','html');
 //在开发过程中，需要取消模板缓存
 swig.setDefaults({cache: false});
 
@@ -28,9 +31,18 @@ app.use('/admin',require('./routers/admin'));
 app.use("/api",require('./routers/api'));
 app.use('/',require('./routers/main'));
 
+//监听http请求
+//连接mongo数据库
+mongoose.connect('mongodb://localhost:27018/blogMiaov',function (err) {
+    if(err){
+        console.log('failed to connect db server');
+    }else{
+        console.log('db connected');
+        //监听端口
+        app.listen(8081)
+    }
+});
 
-//监听端口
-app.listen(8081)
 
 //用户发送http请求 》》url 》》 解析路由 》》 找到匹配的规则 》》 执行指定的绑定函数，返回对应内容至用户
 // public 》》 静态 》》 直接读取指定目录下的文件，返回给用户 》》 动态 》》 处理业务逻辑，加载模板，解析模板 》》 返回数据给用户
